2ee961ff791eead66d9072acccdc6b3c0391562c
mizuki-yone <b00798709@essec.edu>
2022-06-13 11:22:03 +0200

    edited band list page

--- src/routes/band_list.svelte
+++ src/routes/band_list.svelte
@@ -1,5 +1,3 @@
-<h1>This page displays all the bands in form of blocks</h1>
-
 <script context='module'>
     import  supabaseClient  from '../../src/lib/supabase';
     let name, type_of_music, mobile_number, email, number_of_members, website, short_description, type_of_event;
@@ -14,20 +12,28 @@
 </script>
 
 <script>
+import { identity } from 'svelte/internal';
+
+    export let bands;    
+    export let id;
     export let name;
-    export let bands;
     export let type_of_music;
-    export let mobile_number;
-    export let email;
-    export let number_of_members;
-    export let website;
     export let type_of_event;
     export let short_description;
+
 </script>
 
+
+<h1>Available band</h1>
+
+<div class="grid lg:grid-cols-3 gap-8"> 
+    {#each bands as band}
+        <div class="bg-white text-black p-6">
+            <h2>{band.name}</h2>
+            <p>{#if band.type_of_music}{band.type_of_music}{:else}<span class="text-gray-400">No specific type of music</span>{/if}</p>
+            <p>{#if band.type_of_event}{band.type_of_event}{:else}<span class="text-gray-400">No specific type of event</span>{/if}</p>
+            <p><a href="http://localhost:3000/band-{band.id}" class="text-blue-500">show detial </a></p>
+        </div>
+    {/each}
     
-<h1>This is {name} band, please contact them on {email}</h1>
-<p>{bands.length}</p>
-{#each bands as band}
-    <p>{band.name}</p>
-{/each}
+</div>
